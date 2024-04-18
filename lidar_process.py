435f84f9d5eb74f71d38efcee1231ad3e9dd9aca
ThanhLamdev <133113489+vuthanhlam848@users.noreply.github.com>
2024-04-18 17:27:12 +0700

    Update lidar_process.py

--- lidar_process.py
+++ lidar_process.py
@@ -4,6 +4,7 @@ import numpy as np
 # Khởi tạo lidar và list chứa data nhận vào từ lidar
 lidar = RPLidar('COM5',timeout=3,baudrate=115200)
 data = []
+
 # Hàm lấy dữ liệu từ lidar
 def get_data():  
     try:
@@ -18,12 +19,15 @@ def get_data():
         lidar.stop_motor()
         lidar.disconnect()
         print('Lidar disconnected')
+        
+# Hàm tính toán tọa độ x và y với mỗi angle và distance thu được từ lidar
 def compute(angle, distance):
     angle = np.deg2rad(angle)
     x = np.sin(angle)*distance
     y = np.cos(angle)*distance
     return x,y
 xy=[compute(angle,distance) for angle,distance in data]
+
 #Chạy hàm thu dữ liệu
 get_data()
 
